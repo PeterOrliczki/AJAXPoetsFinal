049622614e4be649e740131854a86bf040973a22
PeterOrliczki <peterorliczkiprimary@gmail.com>
2019-05-16 21:03:49 +0200

    Init sql script and data source method fix

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,71 +1,27 @@
 /*
     Database initialization script that runs on every web-application redeployment.
 */
-DROP TABLE IF EXISTS coupons_shops;
-DROP TABLE IF EXISTS coupons;
-DROP TABLE IF EXISTS shops;
-DROP TABLE IF EXISTS users;
+drop table IF EXISTS poems CASCADE;
+drop table IF EXISTS users CASCADE;
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-	CONSTRAINT email_not_empty CHECK (email <> ''),
-	CONSTRAINT password_not_empty CHECK (password <> '')
+create TABLE users (
+    id smallint PRIMARY KEY,
+    name varchar(20) NOT NULL,
+    password TEXT NOT NULL
 );
 
-CREATE TABLE shops (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-	CONSTRAINT name_not_empty CHECK (name <> '')
+create TABLE poems (
+    id smallint PRIMARY KEY,
+    user_id smallint NOT NULL,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE coupons (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    percentage INTEGER NOT NULL,
-    CONSTRAINT name_not_empty CHECK (name <> ''),
-	CONSTRAINT percentage_between_bounds CHECK (percentage >= 0 AND percentage <= 100)
-);
+insert into users values(1, 'Jenő Rejtő', 'jrejto1905'); -- 1
+insert into users values(2, 'DBeaver', 'postgres'); -- 2
 
-CREATE TABLE coupons_shops (
-    coupon_id INTEGER,
-    shop_id INTEGER,
-    PRIMARY KEY (coupon_id, shop_id),
-    FOREIGN KEY (coupon_id) REFERENCES coupons(id),
-    FOREIGN KEY (shop_id) REFERENCES shops(id)
-);
-
-INSERT INTO users (email, password) VALUES
-	('user1@user1', 'user1'), -- 1
-	('user2@user2', 'user2'), -- 2
-	('user2@user3', 'user3'); -- 3
-
-INSERT INTO shops (name) VALUES
-	('SPAR'),   -- 1
-	('Tesco'),  -- 2
-	('Auchan'), -- 3
-	('LIDL'),   -- 4
-	('ALDI');   -- 5
-
-INSERT INTO coupons (name, percentage) VALUES
-	('Sausage discount', 10),           -- 1
-	('Bread super-sale', 50),           -- 2
-	('Bread super-sale', 40),           -- 3
-	('20% off from EVERYTHING!', 20),   -- 4
-	('1 product for FREE!', 100);       -- 5
-
-INSERT INTO coupons_shops (coupon_id, shop_id) VALUES
-    (1, 1), -- 1
-    (1, 2),
-    (1, 3),
-    (2, 1), -- 2
-    (2, 2),
-    (2, 3),
-    (2, 5),
-    (3, 1), -- 3
-    (3, 2),
-    (3, 5),
-    (4, 3), -- 4
-    (5, 2), -- 5
-    (5, 5);
+insert into poems values(3, 1, 'Most megy... megy...', 'És most megy mindég... mindég távolabb <br> Egyhangu uton... busan... csendesen... <br> Amig lassan... lassan... magamra hagy <br> Észrevéttelen. <br> <br> Mi egymástól nagyon messze voltunk <br> Én jajgattam és ő csak kacagott <br> Együtt mi ketten sohasem daloltunk <br> És most megy... megy... ott. <br> <br> Járt a fényben melj engem körülvett <br> Járt a mosoljgott vigan, boldogan <br> Mig én magamban sirtam könnyeket <br> Némán hangtalan. <br> <br> És most megy... mindég... mindég távolabb <br> Egyhangu uton busan csendesen. <br> Amig lassan... lassan... magamra hagy <br> "Az életem."');
+insert into poems values(4, 1, 'Mese...', 'Egy bolond mese lesz a kettönk sorsa <br> Két elhaló, üres bus mese <br> És leszünk mégis egy mese bolondja <br> A mese: minden mese végzete <br> <br> Fölöttünk lesznek uj bolond mesék <br> Az uj mesék bolondjai leszünk <br> Mondunk magunknak hazug rongy mesét <br> S nem vesszük észre majd, hogy szenvedünk <br> <br> Egymásnak nem mesélünk sohasem <br> Külön mesélünk majd a csendes éjnek <br> Ez furcsa lesz, nem tudja senki sem <br> Hogy két halottja lesz egy bus mesének');
+insert into poems values(5, 2, 'Connect to PostgreSQL - postgres', 'FATAL: password authentication failed for user admin');
+insert into poems values(6, 2, 'Connection error', 'Unable to complete network request to host localhost <br> Connection refused: connect');
